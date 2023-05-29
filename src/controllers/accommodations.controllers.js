4e179ba35899bd9226a5684c78f06512e829ec6f
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-05-29 10:10:19 -0300

    feat: rota de adicao de hoteis

--- src/controllers/accommodations.controllers.js
+++ src/controllers/accommodations.controllers.js
@@ -33,3 +33,26 @@ export async function getAccommodationById(req, res) {
         res.status(500).send(error);
     }
 }
+
+export async function postAccommodation(req, res) {
+    const accommodation = req.body;
+    try {
+        const newAccommodation = await db.query(`INSERT INTO accommodations (name, "locationId", "pricePerDay", description)
+        VALUES ($1, $2, $3, $4) RETURNING id`, [accommodation.name, accommodation.locationId, accommodation.pricePerDay, accommodation.description]);
+        accommodation.pictures.forEach(async picture => {
+            await db.query(`INSERT INTO "accommodationsPictures" ("accommodationId", url)
+            VALUES ($1, $2) RETURNING id`, [newAccommodation.rows[0].id, picture]);
+        });
+        accommodation.conveniences.forEach(async convenience => {
+            await db.query(`INSERT INTO "accommodationsConveniences" ("accommodationId", "convenienceId")
+            VALUES ($1, $2)`, [newAccommodation.rows[0].id, convenience]);
+        });
+        await db.query(`UPDATE accommodations
+        SET "mainPictureId" = (SELECT id FROM "accommodationsPictures" WHERE "accommodationId" = $1 LIMIT 1)
+        WHERE id = $1`, [newAccommodation.rows[0].id]);
+        res.status(200).send("Accommodation added");
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
--- src/routers/accommodations.routes.js
+++ src/routers/accommodations.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAccommodationById, getAccommodationsByDestinationId } from "../controllers/accommodations.controllers.js";
+import { getAccommodationById, getAccommodationsByDestinationId, postAccommodation } from "../controllers/accommodations.controllers.js";
 
 
 const accommodationsRouter = Router()
 
 accommodationsRouter.get("/accommodations/:destinationId", getAccommodationsByDestinationId);
 accommodationsRouter.get("/accommodation/:id", getAccommodationById);
+accommodationsRouter.post("/accommodation", postAccommodation);
 
 export default accommodationsRouter;
